cb2011fe187ed15ac2d81427adb1ae3012b898df
Pararcosm Arcana <109973600+Pararcana@users.noreply.github.com>
2023-09-07 12:50:48 +0100

    Update main.lua
    - made the code more readable by seperating out left and right clicking
    - changed the way to verify if the board is solved from using a seperate array to simply using the clicked square array to save memory
    - fixed a memory leak in the clicksquared array by checking for duplicate entries and discarding them (before, it would keep appending to the list)

--- main.lua
+++ main.lua
@@ -1,11 +1,10 @@
 _G.love = require("love")
 
 
-local function isDuplicate(arr, x, y, index)
+local function index(arr, x, y)
     for i = 1, #arr do
         if arr[i][1] == x and arr[i][2] == y then
-            local data = index and i or true
-            return data
+            return i
         end
     end
     return false
@@ -23,18 +22,18 @@ end
 
 local function verify(dt)
     dtotal = dtotal + dt
-    if #verifyArr == 0 and #bombSquares ~= 0 then
+    if #clickedSquares == (boardSize*boardSize) - #bombSquares and #bombSquares ~= 0 then
         sfx["win"]:play()
         level = level + 1
         reset()
     end
     if gameOver then
         if not db then
+            flagSquares = {}
             startSecs = dtotal
             db = true
         end
         if dtotal > startSecs + 3 then
-            verifyArr = {}
             deaths = deaths + 1
             reset()
             gameOver = false
@@ -49,20 +48,15 @@ local function setup()
     math.randomseed(os.time())
     while count ~= bombAmount + 1 do
         num1, num2 = math.random(1, boardSize), math.random(1, boardSize)
-        if not (num1 == rank and num2 == file) and not isDuplicate(bombSquares, rank, file) and not isDuplicate(bombSquares, num1, num2) then
+        if not (num1 == rank and num2 == file) and not index(bombSquares, rank, file) and not index(bombSquares, num1, num2) then
             table.insert(bombSquares, {num1, num2})
             count = count + 1
         end
     end
-    for i = 1, boardSize do for j = 1, boardSize do
-        if not isDuplicate(bombSquares, i, j) then
-            table.insert(verifyArr, {i,j})
-        end end
-    end
     for v = 1, #bombSquares do for i = -1, 1, 1 do for j = -1, 1, 1 do
-        if (i ~= 0 or j ~= 0) and math.max(bombSquares[v][1] + i, bombSquares[v][2] + j) <= boardSize and math.min(bombSquares[v][1] + i, bombSquares[v][2] + j) > 0 and not isDuplicate(bombSquares, bombSquares[v][1] + i, bombSquares[v][2] + j) then
-            if isDuplicate(numSquares, bombSquares[v][1] + i,bombSquares[v][2] + j) and #numSquares ~= 0 then 
-                local w = isDuplicate(numSquares, bombSquares[v][1] + i,bombSquares[v][2] + j, true)
+        if (i ~= 0 or j ~= 0) and math.max(bombSquares[v][1] + i, bombSquares[v][2] + j) <= boardSize and math.min(bombSquares[v][1] + i, bombSquares[v][2] + j) > 0 and not index(bombSquares, bombSquares[v][1] + i, bombSquares[v][2] + j) then
+            if index(numSquares, bombSquares[v][1] + i,bombSquares[v][2] + j) and #numSquares ~= 0 then 
+                local w = index(numSquares, bombSquares[v][1] + i,bombSquares[v][2] + j)
                 numSquares[w][3] = numSquares[w][3] + 1
             else
                 table.insert(numSquares, {bombSquares[v][1] + i, bombSquares[v][2] + j, 1})
@@ -75,12 +69,12 @@ end
 local function drawBoard()
     for x = 1, boardSize do
         for y = 1, boardSize do
-            if isDuplicate(numSquares, x, y) then
-                local num = isDuplicate(numSquares, x, y, true)
+            if index(numSquares, x, y) then
+                local num = index(numSquares, x, y)
                 love.graphics.setColor(love.math.colorFromBytes(colorSchema[numSquares[num][3]][1], colorSchema[numSquares[num][3]][2], colorSchema[numSquares[num][3]][3]))
                 love.graphics.print(numSquares[num][3], x*square+square*0.225, y*square, 0, square/15, square/15)
             end
-            if not isDuplicate(clickedSquares, x, y) then
+            if not index(clickedSquares, x, y) then
                 love.graphics.setColor(1,1,1)
                 local rgb = (x+y+1)%2==0 and {113, 235, 52} or {100, 200, 50}
                 love.graphics.setColor(love.math.colorFromBytes(rgb[1], rgb[2], rgb[3]))
@@ -103,52 +97,43 @@ end
 
 local function click(x, y, button)
     if not gameOver then
-        if math.min(x,y) > square and math.max(x,y) < square * (boardSize+1) then
+        if math.min(x,y) > square and math.max(x,y) < square * (boardSize+1) then -- bounds check for cursor
             rank, file = math.floor(x/square), math.floor(y/square)
-            local arr1 = button == 1 and clickedSquares or flagSquares
-            local arr2 = button ~= 1 and clickedSquares or flagSquares
-            if #clickedSquares == 0 and button == 1 and not isDuplicate(flagSquares, rank, file) then
-                setup()
-            end
-            if (not isDuplicate(arr1, rank, file) or button == 1) and not isDuplicate(arr2, rank, file) then
-                if isDuplicate(bombSquares, rank, file) and button == 1 then
+
+            if button == 1 and not index(flagSquares, rank, file) and not index(clickedSquares, rank, file) then
+                if #clickedSquares == 0 then
+                    setup()
+                elseif index(bombSquares, rank, file) then
                     sfx["boom"]:play()
                     gameOver = true
-                else
-                    if isDuplicate(verifyArr, rank, file) and not isDuplicate(numSquares, rank, file) and button == 1 then
-                        table.insert(clickedSquares, {rank,file})
-                        table.remove(verifyArr, isDuplicate(verifyArr, rank, file, true))
-                        sfx["blip"]:play()
-                        local nilSquares = {{rank, file}}
-                        while #nilSquares ~= 0 do
-                            for i = -1, 1, 1 do for j = -1, 1, 1 do
-                                if i ~= 0 or j ~= 0 and math.max(nilSquares[1][1] + i, nilSquares[1][2] + j) <= boardSize and math.min(nilSquares[1][1] + i, nilSquares[1][2] + j) > 0 then
-                                    if isDuplicate(verifyArr, nilSquares[1][1] + i, nilSquares[1][2] + j) and not isDuplicate(flagSquares, nilSquares[1][1] + i, nilSquares[1][2] + j) and not isDuplicate(numSquares, nilSquares[1][1] + i, nilSquares[1][2] + j)then
-                                        table.insert(nilSquares, {nilSquares[1][1] + i, nilSquares[1][2] + j})
-                                    end
-                                    table.insert(clickedSquares, {nilSquares[1][1] + i, nilSquares[1][2] + j})
-                                    local index = isDuplicate(verifyArr, nilSquares[1][1] + i, nilSquares[1][2] + j, true)
-                                    if type(index) == "number" then
-                                        table.remove(verifyArr, index)
-                                    end
-                                end
-                            end end 
-                            table.remove(nilSquares, 1)
-                        end 
-                    else 
-                        local sound = button == 1 and "blip" or "flag"
-                        sfx[sound]:play()
-                        table.insert(arr1, {rank, file})
-                        if button == 1 then
-                            local index = isDuplicate(verifyArr, rank, file, true)
-                            if type(index) == "number" then
-                                table.remove(verifyArr, index)
-                            end
-                        end
-                    end
                 end
-            elseif isDuplicate(arr1,rank, file) then
-                table.remove(arr1, isDuplicate(arr1,rank,file,true))
+                sfx["blip"]:play()
+                table.insert(clickedSquares, {rank, file})
+
+                if not index(numSquares, rank, file) then
+                    nilSquares = {{rank, file}}
+                    while #nilSquares ~= 0 do
+                        for i = -1, 1 do for j = -1, 1 do
+                            if (i ~= 0 or j ~= 0) and math.max(nilSquares[1][1] + i, nilSquares[1][2] + j) <= boardSize and math.min(nilSquares[1][1] + i, nilSquares[1][2] + j) > 0 then
+                                if not index(clickedSquares, nilSquares[1][1] + i, nilSquares[1][2] + j) and not index(flagSquares, nilSquares[1][1] + i, nilSquares[1][2] + j) and not index(numSquares, nilSquares[1][1] + i, nilSquares[1][2] + j) then
+                                    table.insert(nilSquares, {nilSquares[1][1] + i, nilSquares[1][2] + j})
+                                end
+                                if not index(clickedSquares, nilSquares[1][1] + i, nilSquares[1][2] + j) then
+                                    table.insert(clickedSquares, {nilSquares[1][1] + i, nilSquares[1][2] + j})
+                                end
+                            end
+                        end end 
+                        table.remove(nilSquares, 1)
+                    end 
+                end
+
+            elseif button == 2 and not index(clickedSquares, rank, file) then
+                duplicate = index(flagSquares, rank, file)
+                if duplicate then
+                    table.remove(flagSquares, duplicate)
+                else
+                    table.insert(flagSquares, {rank, file})
+                end
             end
         end
     end
@@ -184,7 +169,8 @@ function love.load()
         ["boom"] = love.audio.newSource("Sounds/Boom.mp3", "static"),
         ["win"] = love.audio.newSource("Sounds/Win.mp3", "static")
     }
-    flagSquares, clickedSquares, bombSquares, verifyArr, numSquares = {}, {}, {}, {}, {}
+    flagSquares, clickedSquares, bombSquares, numSquares = {}, {}, {}, {}
+    sharedVal, nilSquares, duplicate = nil, nil, nil
 end
 
 
@@ -200,5 +186,4 @@ function love.draw()
     drawBoard()
     love.graphics.setColor(1,1,1)
     love.graphics.print("# of bombs = " .. bombAmount .. "    Level = " .. level.. "    Deaths = " .. deaths.. "    Time = "..math.floor(dtotal))
-
 end
